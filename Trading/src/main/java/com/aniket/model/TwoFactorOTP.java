80664a531f92951ea862982ee25da7b68f97c089
aniketjawarkar <aniketnot2026@gmail.com>
2025-11-02 18:22:41 +0530

    Add TwoFactorOTP model to support two-factor authentication

---
+++ Trading/src/main/java/com/aniket/model/TwoFactorOTP.java
@@ -0,0 +1,66 @@
+package com.aniket.model;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class TwoFactorOTP {
+
+    @Id
+    private String id;
+
+    private String otp;
+
+
+    @OneToOne
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private User user;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String jwt;
+
+    public TwoFactorOTP(String id, String otp, User user, String jwt) {
+        this.id = id;
+        this.otp = otp;
+        this.user = user;
+        this.jwt = jwt;
+    }
+
+    public TwoFactorOTP() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getOtp() {
+        return otp;
+    }
+
+    public void setOtp(String otp) {
+        this.otp = otp;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+
+    public void setJwt(String jwt) {
+        this.jwt = jwt;
+    }
+}
