cc3453d7ddd34cfa5fa445ec91cbd2690722e365
aniketjawarkar <aniketnot2026@gmail.com>
2025-11-08 18:28:04 +0530

    feat(service): implement UserService with JWT-based user retrieval and 2FA enablement
    Provides logic for:
    - Finding user by JWT, email, or ID
    - Enabling two-factor authentication
    - Updating user password

---
+++ Trading/src/main/java/com/aniket/Service/UserServiceImpl.java
@@ -0,0 +1,71 @@
+package com.aniket.service;
+
+import com.aniket.config.JwtProvider;
+import com.aniket.domain.VerificationType;
+import com.aniket.model.TwoFactorAuth;
+import com.aniket.model.User;
+import com.aniket.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class UserServiceImpl implements UserService{
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Override
+    public User findUserProfileByJwt(String jwt) throws Exception {
+        String email = JwtProvider.getEmailFromToken(jwt);
+        User user = userRepository.findByEmail(email);
+
+        if(user == null){
+            throw new Exception("User not found");
+
+        }
+        return user;
+    }
+
+    @Override
+    public User findUserByEmail(String email) throws Exception {
+
+        User user = userRepository.findByEmail(email);
+
+        if(user == null){
+            throw new Exception("User not found");
+
+        }
+        return user;
+    }
+
+    @Override
+    public User findUserById(Long userId) throws Exception {
+        Optional<User> user = userRepository.findById(userId);
+        if(user.isEmpty()){
+
+            throw new Exception("User not found");
+        }
+        return user.get();
+    }
+
+    @Override
+    public User enableTwoFactorAuthentication(VerificationType verificationType, String sendTo, User user) {
+        TwoFactorAuth twoFactorAuth = new TwoFactorAuth();
+        twoFactorAuth.setEnabled(true);
+        twoFactorAuth.setSendTo(verificationType);
+        user.setTwoFactorAuth(twoFactorAuth);
+
+        return userRepository.save(user);
+    }
+
+
+    @Override
+    public User updatePassword(User user, String newPassword) {
+        user.setPassword(newPassword);
+        return userRepository.save(user);
+    }
+}
