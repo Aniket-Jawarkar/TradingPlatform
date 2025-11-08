8bb30c68e62f8dabd20ed71192f1a6d5f8334d48
aniketjawarkar <aniketnot2026@gmail.com>
2025-11-08 18:27:49 +0530

    feat(repository): add VerificationCodeRepository for OTP persistence
    Provides method to find verification codes by user ID.

---
+++ Trading/src/main/java/com/aniket/repository/VerificationCodeRepository.java
@@ -0,0 +1,8 @@
+package com.aniket.repository;
+
+import com.aniket.model.VerificationCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VerificationCodeRepository extends JpaRepository<VerificationCode, Long> {
+    public VerificationCode findByUserId(Long userId);
+}
