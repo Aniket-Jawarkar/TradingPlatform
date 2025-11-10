54fcf58c705b1921988760870c90224c95340fac
aniketjawarkar <aniketnot2026@gmail.com>
2025-11-10 12:54:26 +0530

    feat: add endpoints for password reset via OTP
    - Added `/auth/users/reset-password/send-otp` endpoint to send OTP for password reset via email
    - Added `/auth/users/reset-password/verify-otp` endpoint to verify OTP and reset user password
    - Integrated email service for OTP delivery
    - Utilized ForgotPasswordToken service for token management and validation

--- Trading/src/main/java/com/aniket/controller/UserController.java
+++ Trading/src/main/java/com/aniket/controller/UserController.java
@@ -1,16 +1,25 @@
 package com.aniket.controller;
 
 
+import com.aniket.request.ForgotPasswordTokenRequest;
 import com.aniket.domain.VerificationType;
+import com.aniket.model.ForgotPasswordToken;
 import com.aniket.model.User;
 import com.aniket.model.VerificationCode;
+import com.aniket.request.ResetPasswordRequest;
+import com.aniket.response.ApiResponse;
+import com.aniket.response.AuthResponse;
 import com.aniket.service.EmailService;
+import com.aniket.service.ForgotPasswordService;
 import com.aniket.service.UserService;
 import com.aniket.service.VerificationCodeService;
+import com.aniket.utils.OtpUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 public class UserController {
@@ -23,6 +32,9 @@ public class UserController {
 
     @Autowired
     private EmailService emailService;
+
+    @Autowired
+    private ForgotPasswordService forgotPasswordService;
 
 
     private String jwt;
@@ -90,6 +102,68 @@ public class UserController {
 
 
 
+    @PostMapping("/auth/users/reset-password/send-otp")
+    public ResponseEntity<AuthResponse> sendForgotPasswordOtp(
+
+            @RequestBody ForgotPasswordTokenRequest req) throws Exception {
+
+
+        User user = userService.findUserByEmail(req.getSentTo());
+
+        String otp = OtpUtils.generateOTP();
+
+        UUID uuid = UUID.randomUUID();
+
+        String id = uuid.toString();
+
+        ForgotPasswordToken token =forgotPasswordService.findByUser(user.getId());
+
+        if(token == null){
+            token = forgotPasswordService.createToken(user , id , otp, req.getVerificationType()  ,req.getSentTo() );
+        }
+
+        if(req.getVerificationType().equals(VerificationType.EMAIL)){
+            emailService.sendVerificationOtpEmail(
+                    user.getEmail(),
+                    token.getOtp());
+        }
+
+
+        AuthResponse response = new AuthResponse();
+
+        response.setSession(token.getId());
+        response.setSession("Password reset otp sent successfully");
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+
+    }
+
+
+
+
+    @PatchMapping("/auth/users/reset-password/verify-otp/")
+    public ResponseEntity<ApiResponse> resetPassword(
+            @RequestParam String id,
+            @RequestBody ResetPasswordRequest req,
+            @RequestHeader("Authorization") String jwt) throws Exception {
+//        User user = userService.findUserProfileByJwt(jwt);
+
+        ForgotPasswordToken forgotPasswordToken = forgotPasswordService.findById(id);
+
+
+        Boolean isVerified = forgotPasswordToken.getOtp().equals((req.getOtp()));
+
+        if(isVerified){
+            userService.updatePassword(forgotPasswordToken.getUser(), req.getPassword());
+            ApiResponse res = new ApiResponse();
+
+            res.setMessage("Password update succesfully");
+
+            return new ResponseEntity<>(res, HttpStatus.ACCEPTED);
+        }
+
+        throw  new Exception("Wrong otp");
+    }
 
 
 }
