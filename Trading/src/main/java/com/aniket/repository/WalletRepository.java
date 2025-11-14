107a7cdc72a39045258a3cdd91c75533e2a4899d
aniketjawarkar <aniketnot2026@gmail.com>
2025-11-14 15:42:24 +0530

    feat(repository): add WalletRepository with userId finder

---
+++ Trading/src/main/java/com/aniket/repository/WalletRepository.java
@@ -0,0 +1,9 @@
+package com.aniket.repository;
+
+import com.aniket.model.Wallet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WalletRepository extends JpaRepository<Wallet, Long> {
+
+    Wallet findByUserId(Long userId);
+}
