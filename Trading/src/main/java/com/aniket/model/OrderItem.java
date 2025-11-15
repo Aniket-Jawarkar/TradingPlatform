5b14f8747bb26b6156c35299e31a0ca208855b08
aniketjawarkar <aniketnot2026@gmail.com>
2025-11-15 20:19:30 +0530

    feat(order): add OrderItem entity

---
+++ Trading/src/main/java/com/aniket/model/OrderItem.java
@@ -0,0 +1,81 @@
+package com.aniket.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+@Entity
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private double quantity;
+    @ManyToOne
+    private Coin coin;
+    private double buyPrice;
+    private double sellPrice;
+    @JsonIgnore
+    @OneToOne
+    private Order order;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    public Coin getCoin() {
+        return coin;
+    }
+
+    public void setCoin(Coin coin) {
+        this.coin = coin;
+    }
+
+    public double getBuyPrice() {
+        return buyPrice;
+    }
+
+    public void setBuyPrice(double buyPrice) {
+        this.buyPrice = buyPrice;
+    }
+
+    public double getSellPrice() {
+        return sellPrice;
+    }
+
+    public void setSellPrice(double sellPrice) {
+        this.sellPrice = sellPrice;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public OrderItem() {
+    }
+
+    public OrderItem(Long id, double quantity, Coin coin, double buyPrice, double sellPrice, Order order) {
+        this.id = id;
+        this.quantity = quantity;
+        this.coin = coin;
+        this.buyPrice = buyPrice;
+        this.sellPrice = sellPrice;
+        this.order = order;
+    }
+}
