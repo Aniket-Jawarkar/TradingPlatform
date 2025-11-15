a3b367d97fab1f8b8a06cea9575b05586c430bc0
aniketjawarkar <aniketnot2026@gmail.com>
2025-11-15 20:21:09 +0530

    feat(repository): add OrderItemRepository

---
+++ Trading/src/main/java/com/aniket/repository/OrderItemRepository.java
@@ -0,0 +1,7 @@
+package com.aniket.repository;
+
+import com.aniket.model.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+}
