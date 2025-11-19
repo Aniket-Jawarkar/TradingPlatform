28a651d0bd68280e532921ad0c57879d7f9be388
aniketjawarkar <aniketnot2026@gmail.com>
2025-11-19 12:34:37 +0530

    feat(api): add endpoints for creating and fetching user payment details

---
+++ Trading/src/main/java/com/aniket/controller/PaymentDetailsController.java
@@ -0,0 +1,44 @@
+package com.aniket.controller;
+
+
+import com.aniket.model.PaymentDetails;
+import com.aniket.model.User;
+import com.aniket.service.PaymentDetailsService;
+import com.aniket.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class PaymentDetailsController {
+
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private PaymentDetailsService paymentDetailsService;
+
+    @PostMapping("/payment-details")
+    public ResponseEntity<PaymentDetails> addPaymentDetails(
+            @RequestBody PaymentDetails paymentDetailsRequest,
+            @RequestHeader("Authorization") String jwt) throws Exception {
+        User user = userService.findUserProfileByJwt(jwt);
+        PaymentDetails paymentDetails = paymentDetailsService.addPaymentDetails(
+                paymentDetailsRequest.getAccountNumber(),
+                paymentDetailsRequest.getAccountHolderName(),
+                paymentDetailsRequest.getIfsc(),
+                paymentDetailsRequest.getBankName(),
+                user
+        );
+        return new ResponseEntity<>(paymentDetails, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/payment-details")
+    public ResponseEntity<PaymentDetails> getUsersPaymentDetails(
+            @RequestHeader("Authorization") String jwt) throws Exception {
+        User user = userService.findUserProfileByJwt(jwt);
+        PaymentDetails paymentDetails=paymentDetailsService.getUsersPaymentDetails(user);
+        return new ResponseEntity<>(paymentDetails, HttpStatus.CREATED);
+    }
+}
